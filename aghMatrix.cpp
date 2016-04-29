36b0d9cc178e297ebd47f1a5d5e777ace8189611
Ukasshu <mielczareklukasz96@gmail.com>
2016-04-29 19:36:10 +0200

    #5 stworzylem specjalizacje operatora * dla klasy string

--- aghMatrix.cpp
+++ aghMatrix.cpp
@@ -196,7 +196,7 @@ T aghMatrix<T>::operator()(const int row, const int col) {
 }
 
 template<>
-aghMatrix aghMatrix<string>::operator+(constr aghMatrix &matrix) const{
+aghMatrix aghMatrix<string>::operator+(const aghMatrix &matrix) const{
     bool equalRows = this->rows == matrix.getRows();
     bool equalCols = this->cols == matrix.getCols();
     if (!equalRows || !equalCols)
@@ -206,6 +206,25 @@ aghMatrix aghMatrix<string>::operator+(constr aghMatrix &matrix) const{
     for(i=0; i<this->rows; ++i)
         for(j=0; j<this->cols; ++j){
             newMatrix.setItem(i, j, combineCollections(this->getItem(i,j), matrix.getItem(i,j));
+        }
+    return newMatrix;
+}
+
+template<>
+aghMatrix aghMatrix<string>::operator*(const aghMatrix &matrix) const{
+    bool correctDimensions = this->cols == matrix.getRows();
+    if(!correctDimensions)
+        throw aghException(1, "Tried to multiply matrices with wrong dimensions!", __FILE__, __LINE__);
+    aghMatrix <string>newMatrix(this->rows, matrix.getCols());
+    int i, j, k;
+    string tmp;
+    for(i =0; i<this->rows, i++)
+        for(j=0; j<matrix.getCols(); j++){
+            tmp="";
+            for(k=0; k<this->cols; k++){
+                tmp=combineCollections(tmp, combineCollections(this->matrixPtr[i][k], matrix.getItem(k, j)));
+            }
+            newMatrix.setItem(i, j, tmp);
         }
     return newMatrix;
 }
